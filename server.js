96a9ce7d7d6a502b2915a20931cbcf25ab3d0305
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-22 23:02:17 +0100

    clustring in action

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express= require('express');
-const cluster= require('cluster');
-const app= express();
+const express = require("express");
+const cluster = require("cluster");
+const app = express();
 
-function delay(duration){
-    const startTime= Date.now();
-    while (Date.now - startTime < duration) {
-        // event loop is blocked...
-    }
+function delay(duration) {
+  const startTime = Date.now();
+  console.log(startTime);
+  while (Date.now() - startTime < duration) {
+    // event loop is blocked...
+  }
 }
 
+app.get("/", (req, res) => {
+  res.send(`performance example: ${process.pid}`);
+});
 
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`ding ding ding ... ${process.pid}`);
+});
 
-app.get('/',(req, res)=>{
-    res.send(`performance example: ${process.pid}`);
-})
+console.log("running server.js");
 
-app.get("/timer",(req,res)=>{
-    delay(9000);
-    res.send(`ding ding ding ... ${process.pid}`);
-})
-
-if(cluster.isMaster){
-    console.log('master process is started');
-    cluster.fork();
-    cluster.fork();
-}else{
-    console.log('worker has been started ..');
-    app.listen(3000);
+if (cluster.isMaster) {
+  console.log("master process is started");
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log("worker has been started ..");
+  app.listen(3000);
 }
