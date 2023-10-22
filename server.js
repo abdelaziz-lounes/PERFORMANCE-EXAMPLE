4d43b181bafa746393c5175f8802175eb5c936f8
abdelaziz_lounes <abdelazizlounes2@gmail.com>
2023-10-22 21:47:09 +0100

    real life blocking functions

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const app= express();
 function delay(duration){
     const startTime= Date.now();
     while (Date.now - startTime < duration) {
-        
+        // event loop is blocked...
     }
 }
+
+
 
 app.get('/',(req, res)=>{
     res.send("performance example");
 })
 
 app.get("/timer",(req,res)=>{
-    
+    delay(9000);
     res.send("ding ding ding ...")
 })
